957b5f46c54352a195565419a2ed520ee9a67f95
David Douglas <dave@softwaresmoothie.com>
2022-08-10 14:49:09 -0500

    Initial Commit

---
+++ DynamicViews/DynamicViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicViewsApp.swift
+//  DynamicViews
+//
+//  Created by David Douglas on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
